ce19d8c84cfcb04516227305a1b9fc10656a3b39
Aronchenf <44628404+Aronchenf@users.noreply.github.com>
2021-03-26 16:52:11 +0800

    添加欢迎页背景

--- app/src/main/java/com/news/simple_news/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/news/simple_news/ui/weather/WeatherViewModel.kt
@@ -3,17 +3,10 @@ package com.news.simple_news.ui.weather
 import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.news.simple_news.base.BaseViewModel
-import com.news.simple_news.R
-import com.news.simple_news.model.api.API
-import com.news.simple_news.model.api.API.appId
-import com.news.simple_news.model.api.API.appSecret
-import com.news.simple_news.model.api.API.weatherType
 import com.news.simple_news.model.bean.*
 import com.news.simple_news.model.room.RoomHelper
 import com.news.simple_news.util.*
-import kotlinx.coroutines.async
 
 class WeatherViewModel : BaseViewModel() {
 
